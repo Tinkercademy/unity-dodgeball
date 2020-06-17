d83d885ef4bdc41325d1fc90143fc6389a8dd9e9
zyiology <zyiology10000@gmail.com>
2020-06-17 16:35:09 +0800

    Lesson 2 Exercise 1

--- Assets/Scripts/BallExplosionScript.cs
+++ Assets/Scripts/BallExplosionScript.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class BallExplosionScript : MonoBehaviour
 {
 	//create a public GameObject for the explosion object
+	public GameObject explosion;
 
-	
 	//instructions on creating explosions
 	//use the OnCollisionEnter function
 	//use collision.collider.tag to check if what you've collided into is the player
 	//use instantiate to create the explosion
-	
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.tag == "Player") {
+			Instantiate(explosion, transform.position, transform.rotation);
+		}
+	}	
 }
